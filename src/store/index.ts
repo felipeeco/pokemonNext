f8ba392aeed67efefbcd99d6c7a4b205ec53ee9a
Luis Felipe Moreno Rondon <luisfelipe.moreno.contractor@bbva.com>
2024-09-30 23:00:35 -0500

    feat(*): create index store redux

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import counterSlice from "./counter/counterSlice";
+import { useDispatch, TypedUseSelectorHook,  useSelector} from "react-redux";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    counterSlice
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
